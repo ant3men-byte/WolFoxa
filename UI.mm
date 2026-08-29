9ace56e47758de94f9dc8f858b65541b59746f80
ant3men-byte <ant3men@gmail.com>
2026-08-29 15:09:59 +0300

    Update UI.mm

--- UI.mm
+++ UI.mm
@@ -3,10 +3,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface WFOverlayViewController : UIViewController
+#pragma mark - Overlay Root
+
+@interface WFOverlayRootController : UIViewController
 @end
 
-@implementation WFOverlayViewController
+@implementation WFOverlayRootController
 
 - (void)loadView {
 
@@ -22,9 +24,17 @@
 @end
 
 
+#pragma mark - UI Controller
+
 @implementation WFUIController {
     UIWindow *_overlayWindow;
-    UIButton *_button;
+    UIButton *_floatingButton;
+
+    UIView *_panel;
+    UILabel *_statusLabel;
+
+    UITextField *_latitudeField;
+    UITextField *_longitudeField;
 }
 
 + (instancetype)sharedController {
@@ -33,27 +43,26 @@
     static dispatch_once_t onceToken;
 
     dispatch_once(&onceToken, ^{
-        instance = [[WFUIController alloc] init];
+        instance =
+            [[WFUIController alloc] init];
     });
 
     return instance;
 }
 
-#pragma mark - FORCE AUTO START
+#pragma mark - Auto Start
 
 + (void)load {
 
-    NSLog(@"[WolFox] WFUIController +load FIRED");
+    NSLog(@"[WolFox] WFUIController +load");
 
     dispatch_after(
         dispatch_time(
             DISPATCH_TIME_NOW,
-            (int64_t)(2.0 * NSEC_PER_SEC)
+            (int64_t)(1.5 * NSEC_PER_SEC)
         ),
         dispatch_get_main_queue(),
         ^{
-
-        NSLog(@"[WolFox] Starting FORCE UI");
 
         [[WFUIController sharedController]
             installWhenReady];
@@ -66,13 +75,15 @@
 
     NSLog(@"[WolFox] installWhenReady");
 
-    dispatch_async(dispatch_get_main_queue(), ^{
+    dispatch_async(
+        dispatch_get_main_queue(),
+        ^{
 
         [self installOverlay];
     });
 }
 
-#pragma mark - Scene
+#pragma mark - Active Scene
 
 - (UIWindowScene *)activeWindowScene
     API_AVAILABLE(ios(13.0)) {
@@ -109,7 +120,8 @@
                 isKindOfClass:
                     UIWindowScene.class]) {
 
-            return (UIWindowScene *)scene;
+            return
+                (UIWindowScene *)scene;
         }
     }
 
@@ -122,14 +134,11 @@
 
     if (_overlayWindow != nil) {
 
-        _overlayWindow.hidden = NO;
-
-        NSLog(@"[WolFox] Overlay already exists");
+        _overlayWindow.hidden =
+            NO;
 
         return;
     }
-
-    NSLog(@"[WolFox] Creating overlay");
 
     UIWindow *window = nil;
 
@@ -140,9 +149,7 @@
 
         if (scene == nil) {
 
-            NSLog(@"[WolFox] NO WINDOW SCENE");
-
-            [self retryInstallation];
+            [self retryInstall];
 
             return;
         }
@@ -168,26 +175,26 @@
     window.windowLevel =
         UIWindowLevelAlert + 1000.0;
 
-    WFOverlayViewController *controller =
-        [[WFOverlayViewController alloc] init];
+    WFOverlayRootController *root =
+        [[WFOverlayRootController alloc]
+            init];
 
     window.rootViewController =
-        controller;
+        root;
 
-    window.hidden = NO;
+    window.hidden =
+        NO;
 
     _overlayWindow =
         window;
 
-    NSLog(@"[WolFox] Overlay window created");
+    [self buildFloatingButtonOnView:
+        root.view];
 
-    [self createButtonOnView:
-        controller.view];
+    NSLog(@"[WolFox] Overlay installed");
 }
 
-#pragma mark - Retry
-
-- (void)retryInstallation {
+- (void)retryInstall {
 
     dispatch_after(
         dispatch_time(
@@ -199,19 +206,17 @@
 
         if (self->_overlayWindow == nil) {
 
-            NSLog(@"[WolFox] Retrying overlay installation");
-
             [self installOverlay];
         }
     });
 }
 
-#pragma mark - Button
+#pragma mark - Floating Button
 
-- (void)createButtonOnView:
+- (void)buildFloatingButtonOnView:
     (UIView *)view {
 
-    if (_button != nil) {
+    if (_floatingButton != nil) {
         return;
     }
 
@@ -222,28 +227,34 @@
     button.frame =
         CGRectMake(
             20.0,
-            180.0,
-            90.0,
-            90.0
+            170.0,
+            68.0,
+            68.0
         );
 
     button.backgroundColor =
-        UIColor.redColor;
+        [UIColor
+            colorWithRed:0.08
+                   green:0.08
+                    blue:0.10
+                   alpha:0.96];
 
     button.layer.cornerRadius =
-        45.0;
+        34.0;
 
     button.layer.borderWidth =
-        4.0;
+        2.0;
 
     button.layer.borderColor =
-        UIColor.whiteColor.CGColor;
+        [UIColor
+            colorWithWhite:1.0
+                     alpha:0.22].CGColor;
 
     button.layer.shadowOpacity =
-        0.9;
+        0.35;
 
     button.layer.shadowRadius =
-        10.0;
+        8.0;
 
     button.layer.shadowOffset =
         CGSizeMake(
@@ -252,29 +263,22 @@
         );
 
     [button
-        setTitle:@"WF"
+        setTitle:@"🦊"
         forState:UIControlStateNormal];
-
-    [button
-        setTitleColor:
-            UIColor.whiteColor
-        forState:
-            UIControlStateNormal];
 
     button.titleLabel.font =
         [UIFont
-            boldSystemFontOfSize:
-                26.0];
+            systemFontOfSize:32.0];
 
     [button
         addTarget:self
-           action:@selector(buttonPressed)
+           action:@selector(floatingButtonPressed)
  forControlEvents:UIControlEventTouchUpInside];
 
     UIPanGestureRecognizer *pan =
         [[UIPanGestureRecognizer alloc]
             initWithTarget:self
-                    action:@selector(handlePan:)];
+                    action:@selector(handleFloatingPan:)];
 
     [button
         addGestureRecognizer:pan];
@@ -283,44 +287,13 @@
 
     [view bringSubviewToFront:button];
 
-    _button =
+    _floatingButton =
         button;
-
-    NSLog(@"[WolFox] ***** WF BUTTON CREATED *****");
 }
 
-#pragma mark - Button Action
+#pragma mark - Drag Button
 
-- (void)buttonPressed {
-
-    NSLog(@"[WolFox] WF BUTTON PRESSED");
-
-    UIAlertController *alert =
-        [UIAlertController
-            alertControllerWithTitle:
-                @"WolFox"
-                             message:
-                @"WolFox.dylib loaded successfully ✅"
-                      preferredStyle:
-                UIAlertControllerStyleAlert];
-
-    [alert
-        addAction:
-            [UIAlertAction
-                actionWithTitle:@"OK"
-                          style:
-                    UIAlertActionStyleDefault
-                        handler:nil]];
-
-    [_overlayWindow.rootViewController
-        presentViewController:alert
-                     animated:YES
-                   completion:nil];
-}
-
-#pragma mark - Drag
-
-- (void)handlePan:
+- (void)handleFloatingPan:
     (UIPanGestureRecognizer *)gesture {
 
     UIView *button =
@@ -394,6 +367,656 @@
             CGPointZero
               inView:
             parent];
+}
+
+#pragma mark - Open Panel
+
+- (void)floatingButtonPressed {
+
+    if (_panel != nil) {
+
+        [self closePanel];
+
+        return;
+    }
+
+    [self buildPanel];
+}
+
+#pragma mark - Panel
+
+- (void)buildPanel {
+
+    UIView *rootView =
+        _overlayWindow
+            .rootViewController
+            .view;
+
+    if (rootView == nil) {
+        return;
+    }
+
+    CGFloat screenWidth =
+        CGRectGetWidth(
+            rootView.bounds
+        );
+
+    CGFloat panelWidth =
+        MIN(
+            360.0,
+            screenWidth - 28.0
+        );
+
+    CGFloat panelHeight =
+        430.0;
+
+    CGFloat x =
+        (screenWidth -
+         panelWidth) /
+        2.0;
+
+    CGFloat y =
+        120.0;
+
+    UIView *panel =
+        [[UIView alloc]
+            initWithFrame:
+                CGRectMake(
+                    x,
+                    y,
+                    panelWidth,
+                    panelHeight
+                )];
+
+    panel.backgroundColor =
+        [UIColor
+            colorWithRed:0.07
+                   green:0.07
+                    blue:0.09
+                   alpha:0.97];
+
+    panel.layer.cornerRadius =
+        24.0;
+
+    panel.layer.borderWidth =
+        1.0;
+
+    panel.layer.borderColor =
+        [UIColor
+            colorWithWhite:1.0
+                     alpha:0.12].CGColor;
+
+    panel.layer.shadowOpacity =
+        0.35;
+
+    panel.layer.shadowRadius =
+        18.0;
+
+    panel.layer.shadowOffset =
+        CGSizeMake(
+            0.0,
+            8.0
+        );
+
+    [rootView addSubview:panel];
+
+    [rootView bringSubviewToFront:panel];
+
+    _panel =
+        panel;
+
+    [self buildPanelContents];
+}
+
+#pragma mark - Panel Contents
+
+- (void)buildPanelContents {
+
+    if (_panel == nil) {
+        return;
+    }
+
+    CGFloat width =
+        CGRectGetWidth(
+            _panel.bounds
+        );
+
+    UILabel *title =
+        [[UILabel alloc]
+            initWithFrame:
+                CGRectMake(
+                    24.0,
+                    22.0,
+                    width - 80.0,
+                    32.0
+                )];
+
+    title.text =
+        @"WolFox";
+
+    title.textColor =
+        UIColor.whiteColor;
+
+    title.font =
+        [UIFont
+            boldSystemFontOfSize:
+                24.0];
+
+    [_panel addSubview:title];
+
+
+    UIButton *closeButton =
+        [UIButton
+            buttonWithType:
+                UIButtonTypeSystem];
+
+    closeButton.frame =
+        CGRectMake(
+            width - 58.0,
+            16.0,
+            42.0,
+            42.0
+        );
+
+    [closeButton
+        setTitle:@"✕"
+        forState:UIControlStateNormal];
+
+    closeButton.titleLabel.font =
+        [UIFont
+            boldSystemFontOfSize:
+                20.0];
+
+    [closeButton
+        setTitleColor:
+            UIColor.whiteColor
+        forState:
+            UIControlStateNormal];
+
+    [closeButton
+        addTarget:self
+           action:@selector(closePanel)
+ forControlEvents:UIControlEventTouchUpInside];
+
+    [_panel addSubview:closeButton];
+
+
+    UILabel *subtitle =
+        [[UILabel alloc]
+            initWithFrame:
+                CGRectMake(
+                    24.0,
+                    58.0,
+                    width - 48.0,
+                    24.0
+                )];
+
+    subtitle.text =
+        @"Standalone Runtime Controller";
+
+    subtitle.textColor =
+        [UIColor
+            colorWithWhite:1.0
+                     alpha:0.55];
+
+    subtitle.font =
+        [UIFont
+            systemFontOfSize:
+                13.0];
+
+    [_panel addSubview:subtitle];
+
+
+    UILabel *latLabel =
+        [self labelWithText:
+            @"Latitude"
+        frame:
+            CGRectMake(
+                24.0,
+                103.0,
+                width - 48.0,
+                20.0
+            )];
+
+    [_panel addSubview:latLabel];
+
+
+    _latitudeField =
+        [self textFieldWithFrame:
+            CGRectMake(
+                24.0,
+                129.0,
+                width - 48.0,
+                46.0
+            )];
+
+    _latitudeField.placeholder =
+        @"24.7136";
+
+    [_panel
+        addSubview:
+            _latitudeField];
+
+
+    UILabel *lonLabel =
+        [self labelWithText:
+            @"Longitude"
+        frame:
+            CGRectMake(
+                24.0,
+                189.0,
+                width - 48.0,
+                20.0
+            )];
+
+    [_panel addSubview:lonLabel];
+
+
+    _longitudeField =
+        [self textFieldWithFrame:
+            CGRectMake(
+                24.0,
+                215.0,
+                width - 48.0,
+                46.0
+            )];
+
+    _longitudeField.placeholder =
+        @"46.6753";
+
+    [_panel
+        addSubview:
+            _longitudeField];
+
+
+    UIButton *setButton =
+        [self actionButtonWithTitle:
+            @"Set Static Location"
+        frame:
+            CGRectMake(
+                24.0,
+                282.0,
+                width - 48.0,
+                48.0
+            )];
+
+    [setButton
+        addTarget:self
+           action:@selector(setStaticLocationPressed)
+ forControlEvents:UIControlEventTouchUpInside];
+
+    [_panel addSubview:setButton];
+
+
+    UIButton *restoreButton =
+        [UIButton
+            buttonWithType:
+                UIButtonTypeSystem];
+
+    restoreButton.frame =
+        CGRectMake(
+            24.0,
+            342.0,
+            width - 48.0,
+            44.0
+        );
+
+    restoreButton.layer.cornerRadius =
+        12.0;
+
+    restoreButton.backgroundColor =
+        [UIColor
+            colorWithWhite:1.0
+                     alpha:0.07];
+
+    [restoreButton
+        setTitle:@"Restore Default"
+        forState:UIControlStateNormal];
+
+    [restoreButton
+        setTitleColor:
+            UIColor.whiteColor
+        forState:
+            UIControlStateNormal];
+
+    restoreButton.titleLabel.font =
+        [UIFont
+            systemFontOfSize:
+                15.0
+                      weight:
+                UIFontWeightSemibold];
+
+    [restoreButton
+        addTarget:self
+           action:@selector(restoreDefaultPressed)
+ forControlEvents:UIControlEventTouchUpInside];
+
+    [_panel
+        addSubview:
+            restoreButton];
+
+
+    _statusLabel =
+        [[UILabel alloc]
+            initWithFrame:
+                CGRectMake(
+                    24.0,
+                    396.0,
+                    width - 48.0,
+                    24.0
+                )];
+
+    _statusLabel.textColor =
+        [UIColor
+            colorWithWhite:1.0
+                     alpha:0.65];
+
+    _statusLabel.font =
+        [UIFont
+            systemFontOfSize:
+                12.0];
+
+    _statusLabel.numberOfLines =
+        1;
+
+    [_panel
+        addSubview:
+            _statusLabel];
+
+    [self refreshStatus];
+}
+
+#pragma mark - Components
+
+- (UILabel *)labelWithText:
+    (NSString *)text
+                    frame:
+    (CGRect)frame {
+
+    UILabel *label =
+        [[UILabel alloc]
+            initWithFrame:frame];
+
+    label.text =
+        text;
+
+    label.textColor =
+        [UIColor
+            colorWithWhite:1.0
+                     alpha:0.82];
+
+    label.font =
+        [UIFont
+            systemFontOfSize:
+                13.0
+                      weight:
+                UIFontWeightMedium];
+
+    return label;
+}
+
+- (UITextField *)textFieldWithFrame:
+    (CGRect)frame {
+
+    UITextField *field =
+        [[UITextField alloc]
+            initWithFrame:frame];
+
+    field.backgroundColor =
+        [UIColor
+            colorWithWhite:1.0
+                     alpha:0.08];
+
+    field.textColor =
+        UIColor.whiteColor;
+
+    field.keyboardType =
+        UIKeyboardTypeNumbersAndPunctuation;
+
+    field.layer.cornerRadius =
+        12.0;
+
+    field.layer.borderWidth =
+        1.0;
+
+    field.layer.borderColor =
+        [UIColor
+            colorWithWhite:1.0
+                     alpha:0.10].CGColor;
+
+    field.font =
+        [UIFont
+            systemFontOfSize:
+                15.0];
+
+    UIView *padding =
+        [[UIView alloc]
+            initWithFrame:
+                CGRectMake(
+                    0.0,
+                    0.0,
+                    12.0,
+                    1.0
+                )];
+
+    field.leftView =
+        padding;
+
+    field.leftViewMode =
+        UITextFieldViewModeAlways;
+
+    return field;
+}
+
+- (UIButton *)actionButtonWithTitle:
+    (NSString *)title
+                           frame:
+    (CGRect)frame {
+
+    UIButton *button =
+        [UIButton
+            buttonWithType:
+                UIButtonTypeSystem];
+
+    button.frame =
+        frame;
+
+    button.backgroundColor =
+        [UIColor
+            colorWithRed:0.25
+                   green:0.55
+                    blue:1.0
+                   alpha:1.0];
+
+    button.layer.cornerRadius =
+        14.0;
+
+    [button
+        setTitle:title
+        forState:UIControlStateNormal];
+
+    [button
+        setTitleColor:
+            UIColor.whiteColor
+        forState:
+            UIControlStateNormal];
+
+    button.titleLabel.font =
+        [UIFont
+            systemFontOfSize:
+                16.0
+                      weight:
+                UIFontWeightSemibold];
+
+    return button;
+}
+
+#pragma mark - Actions
+
+- (void)setStaticLocationPressed {
+
+    double latitude =
+        [_latitudeField.text
+            doubleValue];
+
+    double longitude =
+        [_longitudeField.text
+            doubleValue];
+
+    WFError *error =
+        [[WFAppManager sharedManager]
+            activateStaticLocationWithLatitude:
+                latitude
+            longitude:
+                longitude];
+
+    if ([error isSuccess]) {
+
+        [self showMessage:
+            @"Static location activated ✅"];
+
+    } else {
+
+        [self showMessage:
+            error.humanReadableMessage.length > 0
+                ? error.humanReadableMessage
+                : @"Unable to activate location"];
+    }
+
+    [self refreshStatus];
+}
+
+- (void)restoreDefaultPressed {
+
+    WFError *error =
+        [[WFAppManager sharedManager]
+            restoreDefaultLocation];
+
+    if ([error isSuccess]) {
+
+        [self showMessage:
+            @"Default location restored ✅"];
+
+    } else {
+
+        [self showMessage:
+            error.humanReadableMessage.length > 0
+                ? error.humanReadableMessage
+                : @"Unable to restore location"];
+    }
+
+    [self refreshStatus];
+}
+
+#pragma mark - Status
+
+- (void)refreshStatus {
+
+    NSDictionary *snapshot =
+        [[WFRuntimeState sharedState]
+            snapshotForUI];
+
+    BOOL enabled =
+        [snapshot[@"locationEnabled"]
+            boolValue];
+
+    double latitude =
+        [snapshot[@"currentLatitude"]
+            doubleValue];
+
+    double longitude =
+        [snapshot[@"currentLongitude"]
+            doubleValue];
+
+    NSString *lastAction =
+        snapshot[@"lastAction"];
+
+    if (enabled) {
+
+        _statusLabel.text =
+            [NSString
+                stringWithFormat:
+                    @"Active: %.6f, %.6f",
+                    latitude,
+                    longitude];
+
+    } else {
+
+        _statusLabel.text =
+            lastAction.length > 0
+                ? lastAction
+                : @"Default environment";
+    }
+}
+
+#pragma mark - Alert
+
+- (void)showMessage:
+    (NSString *)message {
+
+    UIViewController *controller =
+        _overlayWindow
+            .rootViewController;
+
+    if (controller == nil) {
+        return;
+    }
+
+    while (controller
+               .presentedViewController != nil) {
+
+        controller =
+            controller
+                .presentedViewController;
+    }
+
+    UIAlertController *alert =
+        [UIAlertController
+            alertControllerWithTitle:
+                @"WolFox"
+                             message:
+                message ?: @""
+                      preferredStyle:
+                UIAlertControllerStyleAlert];
+
+    [alert
+        addAction:
+            [UIAlertAction
+                actionWithTitle:@"OK"
+                          style:
+                    UIAlertActionStyleDefault
+                        handler:nil]];
+
+    [controller
+        presentViewController:alert
+                     animated:YES
+                   completion:nil];
+}
+
+#pragma mark - Close
+
+- (void)closePanel {
+
+    [_latitudeField
+        resignFirstResponder];
+
+    [_longitudeField
+        resignFirstResponder];
+
+    [_panel
+        removeFromSuperview];
+
+    _panel =
+        nil;
+
+    _statusLabel =
+        nil;
+
+    _latitudeField =
+        nil;
+
+    _longitudeField =
+        nil;
 }
 
 @end
